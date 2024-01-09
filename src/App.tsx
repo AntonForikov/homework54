da1c37290dc0ed9505b5c213cf1a121217077d8f
Anton Forikov <aforikov@platformanetworks.com>
2024-01-09 14:34:01 +0600

    Refactor and clean "app" element

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,10 @@ import Field from "./components/Field/Field.tsx";
 import Counter from "./components/Counter/Counter.tsx";
 import Button from "./components/Button/Button.tsx";
 import Block from "./components/Block/Block.tsx";
-
-interface block {
-    id: number
-    hasItem: boolean,
-    clicked: boolean,
-}
+import {block} from "./types";
 
 const startState = () => {
-    const arr: block[] = []
+    const arr: block[] = [];
     for (let i = 0; i < 36; i++) {
         arr.push({hasItem: false, clicked: false, id: i});
     }
@@ -24,22 +19,16 @@ const startState = () => {
 function App() {
     const [blockList, setBlockList] = useState(startState());
 
-    const counter = () => {
-        const count = blockList.filter((block) => {
-            return block.clicked
-        })
-        return count.length
-    }
     const click = (idx: number) => {
         const blockListCopy = [...blockList];
         blockListCopy[idx].clicked = true;
         setBlockList([...blockListCopy]);
-    }
+    };
 
     const resetGame = () => {
         const blockListCopy = startState();
-        setBlockList([...blockListCopy])
-    }
+        setBlockList([...blockListCopy]);
+    };
 
     const blocks: React.ReactNode = (
         <>
@@ -49,17 +38,17 @@ function App() {
                 })
             }
         </>
-    )
+    );
 
     return (
         <>
-            <Field>
+            <Field winArr={blockList}>
                 {blocks}
             </Field>
-            <Counter counter={counter()}></Counter>
+            <Counter counterArr={blockList}></Counter>
             <Button onClick={resetGame}></Button>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
